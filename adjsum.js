db296f66a5dc6110aa1aa3cf06f6cfd00840abdf
Nikhil <nikhilcares22@gmail.com>
2022-09-05 03:57:56 +0530

    added adj sum problem with dp

--- adjsum.js
+++ adjsum.js
@@ -1,12 +1,29 @@
-//find max sum without taking adj element 
-function findMaxAdjSum(arr, i) {
+//find max sum without taking adj element
+// function findMaxAdjSum(arr, i) {
+// 	if (i < 0) return 0;
+// 	if (i == 0) return arr[i];
+// 	//pick
+// 	let pick = arr[i] + findMaxAdjSum(arr, i - 2);
+// 	//non-pick
+// 	let nonPick = findMaxAdjSum(arr, i - 1);
+
+// 	return Math.max(pick, nonPick);
+// }
+// let arr = [2, 1, 4, 9];
+// console.log(findMaxAdjSum(arr, 3));
+
+function findMaxAdjSum(arr, i, dp = []) {
+	//dynamic programming
 	if (i < 0) return 0;
 	if (i == 0) return arr[i];
-	//pick
-	let pick = arr[i] + findMaxAdjSum(arr, i - 2);
-	//non-pick
-	let nonPick = findMaxAdjSum(arr, i - 1);
+	if (dp[i]) return dp[i]; //if already in dp array return and avoid overlapping subproblems
 
+	//pick
+	let pick = arr[i] + findMaxAdjSum(arr, i - 2, dp);
+	//non-pick
+	let nonPick = findMaxAdjSum(arr, i - 1, dp);
+
+	dp[i] = Math.max(pick, nonPick); //add to dp array
 	return Math.max(pick, nonPick);
 }
 let arr = [2, 1, 4, 9];
