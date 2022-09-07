20ce35565a46680a1ca57a4d0fe729dae1a1f29a
Nikhil <nikhilcares22@gmail.com>
2022-09-08 04:37:58 +0530

    added dp array addition for overlapping subproblems

--- NinjaTraning.js
+++ NinjaTraning.js
@@ -7,6 +7,37 @@
     base case  is  day 0 where we performed last task , so we will take maximum of the tasks which are exempted.
 */
 
+// let points = [
+// 	//   0  1  2 task number
+// 	[2, 1, 3], //1st day
+// 	[3, 4, 6], //2nd day
+// 	[10, 1, 6], //3rd day
+// 	[8, 3, 7], //4th day
+// ];
+
+// function calcMaxPoints(day, last, points) {//recursion
+// 	//base case
+// 	let maxPts = 0;
+// 	if (day === 0) {
+// 		for (let i = 0; i < points[0].length; i++) {
+// 			if (last !== i) {
+// 				maxPts = Math.max(maxPts, points[day][i]);
+// 			}
+// 		}
+// 		return maxPts;
+// 	}
+// 	// recursive case
+// 	for (let i = 0; i < points[0].length; i++) {
+// 		if (last !== i) {
+// 			maxPts = Math.max(
+// 				maxPts,
+// 				points[day][i] + calcMaxPoints(day - 1, i, points)
+// 			);
+// 		}
+// 	}
+// 	return maxPts;
+// }
+
 let points = [
 	//   0  1  2 task number
 	[2, 1, 3], //1st day
@@ -15,15 +46,25 @@ let points = [
 	[8, 3, 7], //4th day
 ];
 
-function calcMaxPoints(day, last, points) {//recursion
+function calcMaxPoints(day, last, points, dp = []) {
+	//handel overlapping subproblems
 	//base case
 	let maxPts = 0;
+
+	if (dp[day] && dp[day][last]) return dp[day][last]; // already present then return from here
+
 	if (day === 0) {
 		for (let i = 0; i < points[0].length; i++) {
 			if (last !== i) {
 				maxPts = Math.max(maxPts, points[day][i]);
 			}
 		}
+		if (!dp[day]) {
+			dp.push([]);
+		} else if (!dp[day][last]) {
+			dp[day][last] = -1;
+		}
+		dp[day][last] = maxPts;
 		return maxPts;
 	}
 	// recursive case
@@ -31,11 +72,17 @@ function calcMaxPoints(day, last, points) {//recursion
 		if (last !== i) {
 			maxPts = Math.max(
 				maxPts,
-				points[day][i] + calcMaxPoints(day - 1, i, points)
+				points[day][i] + calcMaxPoints(day - 1, i, points,dp)
 			);
 		}
 	}
+	if (!dp[day]) {
+		dp.push([]);
+	} else if (!dp[day][last]) {
+        dp[day][last] = -1;
+	}
+	dp[day][last] = maxPts;
 	return maxPts;
 }
 
-console.log(calcMaxPoints(01,1, points));
+console.log(calcMaxPoints(3, 3, points));
