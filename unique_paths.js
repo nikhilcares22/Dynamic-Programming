0bfc8dbe26c892e30356853ae3c70de78fa784a9
Nikhil <nikhilcares22@gmail.com>
2022-09-15 00:55:10 +0530

    recursive solution with dp

--- unique_paths.js
+++ unique_paths.js
@@ -1,10 +1,25 @@
-function findUniquePaths(i, j) {//recursive solution
-	//  f(i,j) represents no. of ways from 0,0 i can reach to i,j
+// function findUniquePaths(i, j) {//recursive solution
+// 	//  f(i,j) represents no. of ways from 0,0 i can reach to i,j
+// 	if (i == 0 && j == 0) return 1;
+// 	if (i < 0 || j < 0) return 0;
+// 	let up = findUniquePaths(i - 1, j);
+// 	let left = findUniquePaths(i, j - 1);
+// 	let result = up + left;
+// 	return result;
+// }
+
+// console.log(findUniquePaths(2, 2));
+
+function findUniquePaths(i, j, dp = []) {
+	//recursive solution with dynamic programming
 	if (i == 0 && j == 0) return 1;
 	if (i < 0 || j < 0) return 0;
-	let up = findUniquePaths(i - 1, j);
-	let left = findUniquePaths(i, j - 1);
+	if (dp[i] && dp[i][j]) return dp[i][j];
+	let up = findUniquePaths(i - 1, j, dp);
+	let left = findUniquePaths(i, j - 1, dp);
 	let result = up + left;
+	if (!dp[i]) dp[i] = [];
+	dp[i][j] = result;
 	return result;
 }
 
